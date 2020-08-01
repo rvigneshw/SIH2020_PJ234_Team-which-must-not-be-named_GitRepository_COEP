3454eb6ed13ce0f459c88a7d8436bcb48612bbbd
upgrade129 <premsanthosh129@outlook.com>
2020-08-01 12:03:05 +0530

    initial commit

---
+++ Android App/app/src/test/java/com/pj234/detectpoorsignal/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.pj234.detectpoorsignal;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
