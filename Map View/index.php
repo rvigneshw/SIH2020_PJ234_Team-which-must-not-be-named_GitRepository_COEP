ab4bbdca36a8d64efd436e3a56cdb432f6ac100e
Vigneshwaran R <vigneshw1811@gmail.com>
2020-08-01 15:32:34 +0530

    Added ability to display data in variable size format

--- Map View/index.php
+++ Map View/index.php
@@ -1,3 +1,20 @@
 <?php
-echo "Hello world";
+$type= $_GET['T'];
+$htmlDOC='<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>SIH Demo</title>
+    <script src="assests/plotly.js"></script>
+    <script src="assests/jquery.min.js"></script>
+</head>
+
+<body>
+    <div id="myDiv">
+        <!-- Plotly chart will be drawn inside this DIV -->
+    </div>
+    <script src="scattermapbox-Green-Variable-Size.js"></script>
+</body>
+</html>';
+echo $htmlDOC;
 ?>
